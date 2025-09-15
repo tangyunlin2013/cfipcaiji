684fb2885c3fa40d08ec910227b977b1d3776374
tangyunlin2013 <138740361+tangyunlin2013@users.noreply.github.com>
2025-09-16 06:33:20 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -25,7 +25,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+        if url == 'https://www.wetest.vip/page/cloudflare/address_v4.html':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
